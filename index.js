88650955edde130f72a09de56d770e8614fca470
+ <rodarte_sarah@yahoo.com>
2022-10-23 14:01:52 -0600

    commit tree 85374f856269d69017b2cbc69c954779ea4789c9

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+const dodger = document.getElementById('dodger')
+dodger.style.backgroundColor = "#FF69B4"
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft()
+  }
+  if (e.which === 39) {
+    moveDodgerRight()
+  }
+})
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace('px', '')
+  const left = parseInt(leftNumbers, 10)
+
+  if (left > 0) {
+    dodger.style.left = `${left - 10}px`
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace('px', '')
+  const left = parseInt(leftNumbers, 10)
+
+  if (left < 360) {
+    dodger.style.left = `${left + 10}px`
+  }
+}
